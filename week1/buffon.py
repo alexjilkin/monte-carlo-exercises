ffd59d7e1b10f689bdd8b7f1ea41da2407ff67c2
Alex Jilkin <jilkinalex@gmail.com>
2023-01-20 20:12:35 +0200

    Finish buffons needle

--- week1/buffon.py
+++ week1/buffon.py
@@ -1,42 +1,57 @@
 import random
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 l = 2
 d = 10
-N = 2
+N = 10
+power = 7
+
+# times = [10, 100, 1000...., 10^7]
+tens_power_arr = np.power(np.full(power, 10, dtype=int), np.arange(1, power + 1))
 
 def main():
-  error_data = np.array([])
+    error_data = np.array([])
 
-  # n = 10, 100 .... 10^7
-  for i in range(1, 7):
-      n = np.power(10, i)
-      error_sum = 0
+    for n in tens_power_arr:
+        error_sum = 0
 
-      # Run same experiment N times
-      for _ in range(0, N):  
-        hit_count = 0
+        # Run same experiment N times
+        for _ in range(0, N):  
+            hit_count = 0
 
-        # Throw needle n times
-        for _ in range(0, n):
-          hit_count += 1 if isNeedleHit(d, l) else 0
+            # Throw needle n times
+            for _ in range(0, n):
+                hit_count += 1 if isNeedleHit(d, l) else 0
 
-        p_hit = hit_count / n
-        pi = 2 * l / (p_hit * d)
-        error_sum += np.abs((np.pi - pi))
-        print("n: {}, pi: {}".format(n, pi))
-      
-      average_error = error_sum / N
-      error_data = np.append(error_data, average_error)
-  print(error_data)    
+            p_hit = hit_count / n
+
+            # If p_hit is 0, just use real pi to not skew results.
+            pi = 2 * l / (p_hit * d) if p_hit != 0 else np.pi
+            error_sum += np.abs((np.pi - pi))
+            print("n: {}, pi: {}".format(n, pi))
+        
+        average_error = error_sum / N
+        error_data = np.append(error_data, average_error)
+    print(error_data)
+    print(tens_power_arr)
+  
+    x = tens_power_arr;
+    y = error_data
+
+    plt.xlabel("Needle throw times")
+    plt.ylabel("Average pi error")
+    plt.xscale('log')
+    plt.scatter(x, y)
+    plt.show()
         
 
 
 def isNeedleHit(d, l): 
-  angle = random.random() * (math.pi / 2)
-  x = random.random() * (d / 2)
+    angle = random.random() * (math.pi / 2)
+    x = random.random() * (d / 2)
 
-  return x <= ((l * np.sin(angle)) / 2)
+    return x <= ((l * np.sin(angle)) / 2)
 
 main()
