70cf27624d599c21187f69cb2ea7ea3253894807
Alex Jilkin <jilkinalex@gmail.com>
2023-02-02 19:55:43 +0200

    Add plot

--- week2/chi_test.py
+++ week2/chi_test.py
@@ -10,17 +10,25 @@ lcg.seed_lcg(1235)
 lcg.seed_pm(12144)
 lcg.seed_twister(4444)
 
-def get_chi():
+# Get chi^2 value based on the RNG type = qcg | lcg | pm | twister; M = number of bins
+# Returns tuple (chi squared, array of bins count)
+def get_chi(type, M):
   N = 1000000
-  M = 100
-  
+  rand = None
+
+  # Use different rand function based on the type giver
+  if type == "qcg":
+    rand =  getattr(qcg, "rand_qcg")
+  else:
+    rand =  getattr(lcg, "rand_{}".format(type))
+    
   bin_size = 1 / M
 
   bins = [[] for i in range(M)]
 
   # Create N random numbers and puts them in bins
   for i in range(0, N):
-    num = lcg.rand_twister()
+    num = rand()
     bin_index = math.floor(num / bin_size)
 
     bins[bin_index].append(num)
@@ -31,10 +39,21 @@ def get_chi():
 
   chi_arr = ((y - E)**2) / E
   chi = np.sum(chi_arr)
-  print(chi)
+  return chi, y
 
-get_chi()
-get_chi()
-get_chi()
-get_chi()
 
+def print_chi_average():
+  chis_array = np.array([get_chi("lcg", 100) for i in range(0, 30)])
+  chi_average = np.average(chis_array)
+  chi_median = np.median(chis_array)
+
+  print("For lcg, the average: {} and the median is: {}".format(chi_average, chi_median))
+
+##
+def plot_bins():
+  chi, y = get_chi("twister", 100)
+
+  plt.bar([i for i in range(1, 101)], y)
+  plt.show()
+
+plot_bins()
