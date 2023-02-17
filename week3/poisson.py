0c0f405462bca70f2eb8d2eb194389716a5a3cfa
Alex Jilkin <jilkinalex@gmail.com>
2023-02-17 17:56:07 +0200

    Add stratified sampling

--- week3/poisson.py
+++ week3/poisson.py
@@ -14,14 +14,22 @@ def direct_sampling(N):
 
   return ((b - a) / N) * samples.sum()
    
-# Returns an array of N stratifed random numbers between a and b
-def stratified_sampling(N, a, b):
+# Returns an array of N partially stratifed in 100 bins random numbers between a and b
+def partially_stratified_sampling(N):
   grids = 32
   samples = []
   size = (b - a) / grids
 
   for i in range(0, grids):
       samples.append([f(random.uniform(i * size, (i + 1) * size)) for _ in range(0, int(N / grids))])
+
+  return ((b - a) / N) * np.array(samples).sum()
+
+# Return an array of stratified random number between b and a
+def stratified_sampling(N):
+  size = (b - a) / N
+
+  samples = [f(random.uniform(i * size, (i + 1) * size)) for i in range(0, int(N))]
 
   return ((b - a) / N) * np.array(samples).sum()
 
@@ -41,4 +49,5 @@ def hit_miss(N):
 
 # print(direct_sampling(N))
 # print(hit_miss(N))
-print(stratified_sampling(N, a, b))
+print(partially_stratified_sampling(N, a, b))
+print(stratified_sampling(N))
